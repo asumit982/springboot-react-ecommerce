ac206442c4d883a62d3a0578ef9359d31620fa39
asumit982 <sumitanshul982@gmail.com>
2025-07-20 12:20:24 +0530

    refactor: integrate Lombok and replace boilerplate with annotations

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -5,7 +5,9 @@ import com.ecommerce.project.model.Category;
 import java.util.List;
 
 public interface CategoryService {
+
     List<Category> getAllCategories();
+
     void createCategory(Category category);
 
     String deleteCategory(Long categoryId);
